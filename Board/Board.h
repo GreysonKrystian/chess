e444dc0bdc0350a03b9defd3d2d3fe25b03fb7ac
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2021-05-21 16:57:10 +0200

    added Board

---
+++ Board/Board.h
@@ -0,0 +1,17 @@
+﻿#include "../Chess/bishop.h"
+#include "../Chess/king.h"
+#include "../Chess/knight.h"
+#include "../Chess/pawn.h"
+#include "../Chess/queen.h"
+#include "../Chess/rook.h"
+#include <array>
+
+class Board
+{
+
+private:
+	std::array<std::array < Figure, 8>, 8> board = {};
+
+public:
+	void SetStartingPostions();
+	void      
