346445e0944fbe92e9fcb8e07c6c1e2d0abc59f0
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2021-05-21 17:36:00 +0200

    changed indexing of board from 1-8 to 0-7 for king and bishop

--- Chess/king.cpp
+++ Chess/king.cpp
@@ -15,8 +15,8 @@ std::list<std::vector<int>> King::get_possible_positions()
 	for (auto itr = all_points.begin(); itr != all_points.end(); ++itr)
 	{
 		bool in_range = true;
-		bool condition1 = ((*itr)[0] < 1 || (*itr)[0] > 8);
-		bool condition2 = ((*itr)[1] < 1 || (*itr)[1] > 8);
+		bool condition1 = ((*itr)[0] < 0 || (*itr)[0] > 7);
+		bool condition2 = ((*itr)[1] < 0 || (*itr)[1] > 7);
 		if (condition1 || condition2)
 		{
 			in_range = false;
--- TestBishop/TestBishop.cpp
+++ TestBishop/TestBishop.cpp
@@ -11,34 +11,34 @@ namespace TestBishop
 		
 		TEST_METHOD(TestBishopAllPossibleMovesMiddle)
 		{
-			Bishop bishop(0, 5, 5);
+			Bishop bishop(0, 4, 4);
 			std::list<std::vector<int>> positions = bishop.get_possible_positions();
 			positions.sort();
-			std::list<std::vector<int>> expected = { {1,1},{2,2},{2,8},{3,3},{3,7},{4,4},{4,6},{6,4},{6,6},{7,3},{7,7},{8,2},{8,8} };
+			std::list<std::vector<int>> expected = { {0,0},{1,1},{1,7},{2,2},{2,6},{3,3},{3,5},{5,3},{5,5},{6,2},{6,6},{7,1},{7,7} };
 			Assert::IsTrue(positions == expected);
 		}
 		TEST_METHOD(TestBishopAllPossibleMovesBottomRightCorner)
 		{
-			Bishop bishop(0, 8, 8);
+			Bishop bishop(0, 7, 7);
+			std::list<std::vector<int>> positions = bishop.get_possible_positions();
+			positions.sort();
+			std::list<std::vector<int>> expected = { {0,0},{1,1},{2,2},{3,3},{4,4},{5,5},{6,6} };
+			Assert::IsTrue(positions == expected);
+		}
+		TEST_METHOD(TestBishopAllPossibleMovesTopLeftCorner)
+		{
+			Bishop bishop(0, 0, 0);
 			std::list<std::vector<int>> positions = bishop.get_possible_positions();
 			positions.sort();
 			std::list<std::vector<int>> expected = { {1,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7} };
 			Assert::IsTrue(positions == expected);
 		}
-		TEST_METHOD(TestBishopAllPossibleMovesTopLeftCorner)
-		{
-			Bishop bishop(0, 1, 1);
-			std::list<std::vector<int>> positions = bishop.get_possible_positions();
-			positions.sort();
-			std::list<std::vector<int>> expected = { {2,2},{3,3},{4,4},{5,5},{6,6},{7,7},{8,8} };
-			Assert::IsTrue(positions == expected);
-		}
 		TEST_METHOD(TestBishopPossibleMovesBottomLeftCorner)
 		{
-			Bishop bishop(0, 1, 8);
+			Bishop bishop(0, 0, 7);
 			std::list<std::vector<int>> positions = bishop.get_possible_positions();
 			positions.sort();
-			std::list<std::vector<int>> expected = { {2,7},{3,6},{4,5},{5,4},{6,3},{7,2},{8,1} };
+			std::list<std::vector<int>> expected = { {1,6},{2,5},{3,4},{4,3},{5,2},{6,1},{7,0} };
 			Assert::IsTrue(positions == expected);
 		}
 	};
