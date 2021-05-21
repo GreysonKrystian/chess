01202722a4bab77bfbe07f5daf1f581e24fd6446
Aleksander Woś <01158332@pw.edu.pl>
2021-05-21 17:36:22 +0200

    Corrected wrong implementation of starting point - (0,0) instead of (1,1)

--- TestQueen/TestQueen.cpp
+++ TestQueen/TestQueen.cpp
@@ -12,30 +12,30 @@ namespace UnitTest2
 
 		TEST_METHOD(bottom_left_corner)
 		{
-			Queen hetman(1, 1, 1);
+			Queen hetman(1, 0, 0);
 			std::list<std::vector<int>> pos = hetman.get_possible_positions();
 			pos.sort();
-			std::list<std::vector<int>> expected_pos = { {1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{1,8},{2,1},{3,1},{4,1},{5,1},{6,1},{7,1},{8,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7},{8,8} };
+			std::list<std::vector<int>> expected_pos = { {0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0},{1,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7} };
 			expected_pos.sort();
 			Assert::IsTrue(pos == expected_pos);
 		}		
 		
 		TEST_METHOD(upper_right_corner)
 		{
-			Queen hetman(1, 8, 8);
+			Queen hetman(1, 7, 7);
 			std::list<std::vector<int>> pos = hetman.get_possible_positions();
 			pos.sort();
-			std::list<std::vector<int>> expected_pos = { {1,8},{2,8},{3,8},{4,8},{5,8},{6,8},{7,8},{8,1},{8,2},{8,3},{8,4},{8,5},{8,6},{8,7},{1,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7} };
+			std::list<std::vector<int>> expected_pos = { {0,7},{1,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,0},{7,1},{7,2},{7,3},{7,4},{7,5},{7,6},{0,0},{1,1},{2,2},{3,3},{4,4},{5,5},{6,6} };
 			expected_pos.sort();
 			Assert::IsTrue(pos == expected_pos);
 		}
 
 		TEST_METHOD(center)
 		{
-			Queen hetman(1, 4, 5);
+			Queen hetman(1, 3, 4);
 			std::list<std::vector<int>> pos = hetman.get_possible_positions();
 			pos.sort();
-			std::list<std::vector<int>> expected_pos = { {5,5},{3,5},{4,6},{4,4},{6,5},{2,5},{4,7},{4,3},{7,5},{1,5},{4,8},{4,2},{8,5},{4,1},{1,2},{2,3},{3,4},{5,4},{6,3},{7,2},{8,1},{3,6},{2,7},{1,8},{5,6},{6,7},{7,8} };
+			std::list<std::vector<int>> expected_pos = { {4,4},{2,4},{3,5},{3,3},{5,4},{1,4},{3,6},{3,2},{6,4},{0,4},{3,7},{3,1},{7,4},{3,0},{0,1},{1,2},{2,3},{4,3},{5,2},{6,1},{7,0},{2,5},{1,6},{0,7},{4,5},{5,6},{6,7} };
 			expected_pos.sort();
 			Assert::IsTrue(pos == expected_pos);
 		}
--- TestRook/TestRook.cpp
+++ TestRook/TestRook.cpp
@@ -22,7 +22,7 @@ namespace UnitTest1
 
 		TEST_METHOD(bottom_right_corner)
 		{
-			Rook wieza(1, 8, 1);
+			Rook wieza(1, 7, 0);
 			std::list<std::vector<int>> pos = wieza.get_possible_positions();
 			pos.sort();
 			std::list<std::vector<int>> expected_pos = { {0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,1},{7,2},{7,3},{7,4},{7,5},{7,6},{7,7} };
@@ -31,28 +31,28 @@ namespace UnitTest1
 
 		TEST_METHOD(upper_left_corner)
 		{
-			Rook wieza(1, 1, 8);
+			Rook wieza(1, 0, 7);
 			std::list<std::vector<int>> pos = wieza.get_possible_positions();
 			pos.sort();
-			std::list<std::vector<int>> expected_pos = { {1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{2,8},{3,8},{4,8},{5,8},{6,8},{7,8},{8,8} };
+			std::list<std::vector<int>> expected_pos = { {0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{1,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,7} };
 			Assert::IsTrue(pos == expected_pos);
 		}
 
 		TEST_METHOD(upper_right_corner)
 		{
-			Rook wieza(1, 8, 8);
+			Rook wieza(1, 7, 7);
 			std::list<std::vector<int>> pos = wieza.get_possible_positions();
 			pos.sort();
-			std::list<std::vector<int>> expected_pos = { {1,8},{2,8},{3,8},{4,8},{5,8},{6,8},{7,8},{8,1},{8,2},{8,3},{8,4},{8,5},{8,6},{8,7} };
+			std::list<std::vector<int>> expected_pos = { {0,7},{1,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,0},{7,1},{7,2},{7,3},{7,4},{7,5},{7,6} };
 			Assert::IsTrue(pos == expected_pos);
 		}
 
 		TEST_METHOD(center)
 		{
-			Rook wieza(1, 4, 5);
+			Rook wieza(1, 3, 4);
 			std::list<std::vector<int>> pos = wieza.get_possible_positions();
 			pos.sort();
-			std::list<std::vector<int>> expected_pos = { {5,5},{3,5},{4,6},{4,4},{6,5},{2,5},{4,7},{4,3},{7,5},{1,5},{4,8},{4,2},{8,5},{4,1} };
+			std::list<std::vector<int>> expected_pos = { {4,4},{2,4},{3,5},{3,3},{5,4},{1,4},{3,6},{3,2},{6,4},{0,4},{3,7},{3,1},{7,4},{3,0} };
 			expected_pos.sort();
 			Assert::IsTrue(pos == expected_pos);
 		}
