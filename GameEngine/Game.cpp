f477fea5ac842731d627d47c18527d628bd28d35
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2021-05-24 00:18:52 +0200

    engine development, moved castling to game engine

--- GameEngine/Game.cpp
+++ GameEngine/Game.cpp
@@ -1,18 +1,99 @@
 ﻿#include "Game.h"
 
 
-Board::Board()
+Game::Game()
 {
+	is_white_turn = true;
 }
 
 
-void capture_figure(int x, int y)
+void Game::capture_figure(int x, int y)
 {
- 	Figure* captured = my_board.get_(x, y);
+ 	Figure* captured = board.get_figure(x, y);
 	delete captured;
 }
 
-void check_win_condition(int current_color)
+bool Game::check_win_condition(int current_color)
 {
-	
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+bool Game::castling_left_conditions(int pos_x, int pos_y)
+{
+
+
+
+	if (board.get_figure(pos_x - 4, pos_y) == nullptr)
+		return false;
+	else
+	{
+		Figure* corner_figure = board.get_figure(pos_x - 4, pos_y);
+		if ((corner_figure->get_type() == "R") && (corner_figure->get_first_move() == true))
+			return true;
+		return false;
+	}
+}
+
+bool Game::castling_right_conditions(int pos_x, int pos_y)
+{
+	if (board.get_figure(pos_x + 3, pos_y) == nullptr)
+		return false;
+	else
+	{
+		Figure* corner_figure = board.get_figure(pos_y + 3, pos_y);
+		if ((corner_figure->get_type() == "R") && (corner_figure->get_first_move() == true))
+			return true;
+		return false;
+	}
+}
+
+std::list<std::vector<int>> Game::get_castling_positions()
+{
+	int pos_x = 4;
+	int pos_y = 0;
+	if (get_current_player() == true)
+	{
+		int pos_x = 4;
+		int pos_y = 7;
+	}
+	std::list<std::vector<int>> castling_positions;
+
+	if ((castling_left_conditions(pos_x, pos_y) == true) && (board.get_figure(pos_x - 1, pos_y) == nullptr) && (board.get_figure(pos_x - 2, pos_y) == nullptr)
+		&& board.get_figure(pos_x - 3, pos_y))
+	{
+		castling_positions.push_back({ pos_x - 2, pos_y });
+	}
+	if ((castling_right_conditions(pos_x, pos_y) == true) && (board.get_figure(pos_x + 1, pos_y)) == nullptr && (board.get_figure(pos_x + 2, pos_y) == nullptr))
+	{
+		castling_positions.push_back({ pos_x + 2, pos_y });
+	}
+
+	return castling_positions;
+}
+
+bool Game::get_current_player()
+{
+	return is_white_turn;
+}
+
+void Game::change_turn()
+{
+	if (is_white_turn == true)
+	{
+		is_white_turn = false;
+	}
+	else
+	{
+		is_white_turn = true;
+	}
 }
--- GameEngine/Game.h
+++ GameEngine/Game.h
@@ -4,15 +4,18 @@
 class Game
 {
 private:
-	Board my_board;
+	Board board;
+	bool is_white_turn;
 public:
 	Game();
-	void capture_figure();
+	void capture_figure(int x, int y);
 	bool check_win_condition(int current_color);
 	void change_turn();
-	bool castling_left_conditions(Board& board);
-	bool castling_right_conditions(Board& board);
-	std::list<std::vector<int>> get_castling_positions(Board& board);
+	bool castling_left_conditions(int pos_x, int pos_y);
+	bool castling_right_conditions(int pos_x, int pos_y);
+	std::list<std::vector<int>> get_castling_positions();
 
+
+	bool get_current_player(); // sprawdz ktory kolor ma ruch bialy = true, czarny false
 	
 };
